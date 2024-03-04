69ce8ee2d53414833f6f612d36a553b9c7552400
isaias-rojas <s_isaiasr@tutanota.com>
2024-03-04 01:49:15 -0400

    fix: solve percentaje of common words

--- src/TextSimilarityChecker.java
+++ src/TextSimilarityChecker.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 public class TextSimilarityChecker {
+
     public static String readFile(String filePath) throws IOException {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -13,22 +12,38 @@ public class TextSimilarityChecker {
                 content.append(line).append("\n");
             }
         }
-        return content.toString().toLowerCase();
+        return content.toString();
     }
 
     public static double calculateSimilarity(String text1, String text2) {
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for (int i = 1; i <= text1.length(); i++) {
-            for (int j = 1; j <= text2.length(); j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+        String[] words1 = text1.split("\\s+");
+        String[] words2 = text2.split("\\s+");
+
+        int[][] dp = new int[words1.length + 1][words2.length + 1];
+
+        for (int i = 1; i <= words1.length; i++) {
+            for (int j = 1; j <= words2.length; j++) {
+                if (words1[i - 1].equalsIgnoreCase(words2[j - 1])) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    int levenshteinDistance = calculateLevenshteinDistance(words1[i - 1].toLowerCase(), words2[j - 1].toLowerCase());
+                    if (levenshteinDistance >= 55) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
                 }
             }
         }
-        return (double) dp[text1.length()][text2.length()] / text1.length() * 100;
+
+        int maxLen = Math.max(words1.length, words2.length);
+        int commonWords = dp[words1.length][words2.length];
+        int nonMatchingWords = Math.max(words1.length - commonWords, words2.length - commonWords);
+        System.out.println("non matching words:" + nonMatchingWords);
+        System.out.println("common words: " + commonWords);
+        return (double) (commonWords * 100) / words2.length;
     }
+
 
     public static void identifyMisspelledWords(String text1, String text2) {
         String[] sentences1 = text1.split("\\.\\s*");
@@ -50,12 +65,12 @@ public class TextSimilarityChecker {
         String[] words2 = sentence2.split("\\s+");
 
         for (String word2 : words2) {
-            boolean foundMatch = false;
             for (String word1 : words1) {
-                if (word1.equals(word2)) continue;
-                if (calculateLevenshteinDistance(word1, word2) >= 55) {
+                if (word1.equalsIgnoreCase(word2)) {
+                    break;
+                }
+                if (calculateLevenshteinDistance(word1.toLowerCase(), word2.toLowerCase()) >= 55) {
                     System.out.println(word2 + " - " + word1);
-                    foundMatch = true;
                     break;
                 }
             }
@@ -86,7 +101,7 @@ public class TextSimilarityChecker {
             String text1 = readFile("/home/isaverse/IdeaProjects/ProjectAlgoI/file1.txt");
             String text2 = readFile("/home/isaverse/IdeaProjects/ProjectAlgoI/file2.txt");
 
-            double similarityPercentage = calculateSimilarity(text2, text1);
+            double similarityPercentage = calculateSimilarity(text1, text2);
             System.out.println("Similarity Percentage: " + similarityPercentage + "%");
 
             identifyMisspelledWords(text1, text2);
